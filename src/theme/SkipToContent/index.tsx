67b17ec2daacfdd78ccb716e2ae05d503081e9bb
Ahmad Rulim <mfalkia342@gmail.com>
2022-10-26 18:01:29 +0700

    Customise skip to content button style

---
+++ src/theme/SkipToContent/index.tsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import Translate, { translate } from '@docusaurus/Translate';
+import { useSkipToContent } from '@docusaurus/theme-common/internal';
+
+export default function SkipToContent() {
+  const { containerRef, handleSkip } = useSkipToContent();
+
+  return (
+    <div
+      ref={containerRef}
+      role="region"
+      aria-label={translate({ id: 'theme.common.skipToMainContent' })}
+    >
+      <a
+        href="#"
+        className="button absolute top-2 -left-96 z-[-99] focus:left-2 focus:z-[250] active:left-2 active:z-50"
+        onClick={handleSkip}
+      >
+        <Translate
+          id="theme.common.skipToMainContent"
+          description="The skip to content label used for accessibility, allowing to rapidly navigate to main content with keyboard tab/enter navigation"
+        >
+          Skip to main content
+        </Translate>
+      </a>
+    </div>
+  );
+}
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@
 
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./src/pages/**/*.{ts,tsx}', './src/components/**/*.{ts,tsx}'],
+  content: [
+    './src/pages/**/*.{ts,tsx}',
+    './src/components/**/*.{ts,tsx}',
+    './src/theme/**/*.{ts,tsx}',
+  ],
   darkMode: ['class', '[data-theme="dark"]'],
   corePlugins: {
     preflight: false,
