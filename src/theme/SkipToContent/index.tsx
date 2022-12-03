b29bf34b13448216b5fdae3ce0f4109955300321
Ahmad Rulim <personal@rulim34.dev>
2022-12-03 16:47:04 +0700

    src: Reswizzle skip to content element
    Signed-off-by: Ahmad Rulim <personal@rulim34.dev>

--- src/theme/SkipToContent/index.tsx
+++ src/theme/SkipToContent/index.tsx
@@ -1,29 +1,8 @@
 import React from 'react';
-import Translate, { translate } from '@docusaurus/Translate';
-// @ts-ignore
-import { useSkipToContent } from '@docusaurus/theme-common/internal';
+import { SkipToContentLink } from '@docusaurus/theme-common';
 
-export default function SkipToContent() {
-  const { containerRef, handleSkip } = useSkipToContent();
-
+export default function SkipToContent(): JSX.Element {
   return (
-    <div
-      ref={containerRef}
-      role="region"
-      aria-label={translate({ id: 'theme.common.skipToMainContent' })}
-    >
-      <a
-        href="#"
-        className="button absolute top-2 -left-96 z-[-99] focus:left-2 focus:z-[250] active:left-2 active:z-50"
-        onClick={handleSkip}
-      >
-        <Translate
-          id="theme.common.skipToMainContent"
-          description="The skip to content label used for accessibility, allowing to rapidly navigate to main content with keyboard tab/enter navigation"
-        >
-          Skip to main content
-        </Translate>
-      </a>
-    </div>
+    <SkipToContentLink className="button fixed top-2 -left-96 z-[-99] focus:left-2 focus:z-[250] active:left-2 active:z-50" />
   );
 }
